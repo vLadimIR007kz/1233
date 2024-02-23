bbf420fb9bf4e8b4f717a6fc7bd17a107ea6c9ad
vLadimIR007kz <nazarenko.v.v2000@gmail.com>
2024-02-23 20:45:42 +0600

    commint

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     login = db.Column(db.String(30), nullable=False, unique=True)
+    teacher=db.Column(db.Integer)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)[0:15]
